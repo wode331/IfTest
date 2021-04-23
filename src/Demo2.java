1a8381d1f31b76a0cbb0665e0dfde746dbab980a
wode331 <752494484@qq.com>
2021-04-23 16:06:17 +0800

    version4:修改了Demo2

--- src/Demo2.java
+++ src/Demo2.java
@@ -8,9 +8,6 @@ import java.awt.*;
 public class Demo2 extends JFrame {
     public static void main(String[] args) {
         System.out.println("dd");
-
-
-
-
+        System.out.println("abc");
     }
 }
